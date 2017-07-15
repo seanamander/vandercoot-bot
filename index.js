72a368a0e09b28bd58781bba6788da91148c236e
seanamander <brohonch@gmail.com>
2017-07-15 02:27:32 -0400

    update index.js
    f

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const Discord = require("Discord");
-const client = new Discord.Client();
+const Discord = require("Discord.js");
+const bot = new Discord.Client();
 
-client.on('message', message => {
-  let args = message.content.split(" ").slice(1);
-  let user = message.author.username;
-  var now = new Date();
-  var date = "[" + now + "]";
-  let guild = message.guild;
-  var serverName = guild.name;
-  console.log(date + serverName.toUpperCase() + " " + user + ": " + message.content)
+bot.on('ready', () => {
+    console.log('loaded BOT'); //logs in the console the bot has loaded
+});
+
+bot.on('ready', () => {
+    bot.user.setGame('%help') //sets the game to %help on launch
+})
+
+bot.on('messageDelete', message => {
+    let args = message.content.split(" ").slice(1);
+    var now = new Date();
+    var date = "[" + now + "]: ";
+    var serverName = "[" + message.guild.name + "]: ";
+    let guild = message.guild;
+    let content = args.join(" ");
+    let user = message.author.username;
+    console.log(date + serverName.toUpperCase() + "[DELETED]: " + user + ": " + message.content)
+})
+
+bot.on('message', message => {
+    let args = message.content.split(" ").slice(1);
+    var now = new Date();
+    var date = "[" + now + "]: ";
+    var serverName = "[" + message.guild.name + "]: ";
+    let guild = message.guild;
+    let content = args.join(" ")
+    let user = message.author.username
+    console.log(date + serverName.toUpperCase() + user + ": " + message.content)
 
   if(message.author.bot) return;
   let command = message.content.split(" ")[0];
@@ -17,11 +37,4 @@ client.on('message', message => {
   }
 });
 
-cient.on('messageDelete', message => {
-  let user = message.author.username;
-  var now = new Date();
-  var date = "[" + now + "]";
-  let guild = message.guild;
-  var serverName = guild.name;
-  console.log("[DELETED] " + date + serverName.toUpperCase() + " " + user + ": " + message.content)
-});
+bot.login("MzM0MDk3NzYzNDYzNTkzOTg0.DEWQTg.CCEmUGc0KJKkKfX9iIy8zOI4ymE");
