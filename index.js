602260e881eb6f4848513ef1eac00c7ae897747e
seanamander <brohonch@gmail.com>
2017-07-15 16:39:07 -0400

    7 cmds

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("Discord.js");
 const bot = new Discord.Client();
-var commands = ["%help", "%newchannel", "%kick", "%ban", "%mute", "%unmute"];
+var commands = ["%help", "%newchannel", "%kick", "%ban", "%mute", "%unmute", "%clear"];
 var blue = 3447003;
 
 bot.on('ready', () => {
@@ -37,6 +37,13 @@ bot.on('message', message => {
   if(message.author.bot) return;
   let command = message.content.split(" ")[0];
 
+  var deleteBotMsg = function(msg) {
+    message.reply(msg)
+        .then(m => {
+            m.delete(3000)
+        })
+  }
+
   if(command === commands[0]) { //fixed
     const embed = new Discord.RichEmbed()
         .setTitle('Help/Commands')
@@ -59,9 +66,12 @@ bot.on('message', message => {
         return;
       }
       guild.createChannel(name, "text").then(console.log("Successfully created channel " + name)).catch(console.error);
-      message.reply("Successfully created channel " + name);
+      deleteBotMsg("Successfully created channel " + name)
     } else {
-      message.reply("Invalid usage, try %help.");
+      message.reply(`Invalid usage, try %help.`)
+          .then(m => {
+              m.delete(3000)
+          })
     }
   }
 
@@ -70,54 +80,54 @@ bot.on('message', message => {
       let modRole = message.guild.roles.find("name", "Moderator"); //gets the role Moderator
       let adminRole = message.guild.roles.find("name", "Admin"); //gets the role admin
       if(!kickMember) { //checks if the user to kick is real
-        message.reply("That's an invalid user.");
+        deleteBotMsg("That's not a valid user.")
         return;
       }
       if(kickMember.length === 0) { //checks if they specified a user to kick
-        message.reply("You need to specify a user to kick.");
+        deleteBotMsg("You need to specify a user to kick.")
         return;
       }
       if(!kickMember.kickable) { //checks if the user specified can be kicked by the user trying to kick them
-        message.reply("You can't kick that user.")
+        deleteBotMsg("You can't kick that user.")
         return;
       }
       if(!message.member.roles.has(modRole.id || adminRole.id) && !message.author.id === message.guild.ownerID) { //checks if the user kicking has the modRole or adminRole or is the owner
-        message.reply("You don't have the sufficient permissions.");
+        deleteBotMsg("You don't have the sufficient permissions.")
         return;
       }
       if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) { //checks if the bot has permission to kick
-        message.reply("I don't have the sufficient permissions.");
+        deleteBotMsg("I don't have the sufficient permissions.")
         return;
       }
     kickMember.kick() //kicks the member
-    message.reply(`Successfully kicked ${kickMember}.`)\n.then(m => { m.delete(3000) })
+    deleteBotMsg("Successfully kicked " + kickMember)
   }
 
   if(command === commands[3]) {
       let banMember = message.mentions.members.first() //member to be banned
       let adminRole = message.guild.roles.find("name", "Admin"); //gets the role admin
       if(!banMember) { //checks if the user to ban is real
-        message.reply("That's an invalid user.");
+        deleteBotMsg("That's not a valid user.")
         return;
       }
       if(banMember.length === 0) { //checks if they specified a user to ban
-        message.reply("You need to specify a user to ban.");
+        deleteBotMsg("You need to specify a user to ban.")
         return;
       }
       if(!banMember.bannable) { //checks if the user specified can be ban by the user trying to ban them
-        message.reply("You can't ban that user.")
+        deleteBotMsg("You can't ban that user.")
         return;
       }
       if(!message.member.roles.has(adminRole.id) && !message.author.id === message.guild.ownerID) { //checks if the user ban has the adminRole or is the owner
-        message.reply("You don't have the sufficient permissions.");
+        deleteBotMsg("You don't have the sufficient permissions.")
         return;
       }
       if(!message.guild.member(bot.user).hasPermission("BAN_MEMBERS")) { //checks if the bot has permission to kick
-        message.reply("I don't have the sufficient permissions.");
+        deleteBotMsg("I don't have the sufficient permissions.")
         return;
       }
     banMember.ban() //kicks the member
-    message.reply(`Successfully banned ${banMember}.`)\n.then(m => { m.delete(3000) })
+    deleteBotMsg("Successfully banned " + banMember)
   }
 
   if(command === commands[4]) {
@@ -126,21 +136,21 @@ bot.on('message', message => {
     let mutedRole = message.guild.roles.find("name", "Muted");
     let muteMember = message.mentions.members.first();
     if(!muteMember) {
-      message.reply("That's an invalid user.");
+      deleteBotMsg("That's not a valid user.")
       return;
     }
     if(muteMember.length === 0) {
-      message.reply("Please specify a user to mute.");
+      deleteBotMsg("Please specify a user to mute.")
     }
     if(!message.member.roles.has(modRole.id||adminRole.id) && !message.author.id === message.guild.ownerID) {
-      message.reply("You don't have the sufficient permissions")
+      deleteBotMsg("You don't have the sufficient permissions.")
       return;
     }
     if(!message.guild.member(bot.user).hasPermission("MANAGE_ROLES")) {
-      message.reply("I don't have the sufficient permissions");
+      deleteBotMsg("I don't have the sufficient permissions.")
     }
     muteMember.addRole(mutedRole);
-    message.reply(`Successfully muted ${muteMember}.`)\n.then(m => { m.delete(3000) })
+    deleteBotMsg("Successfully muted " + muteMember)
   }
 
   if (command === commands[5]) {
@@ -149,21 +159,41 @@ bot.on('message', message => {
     let mutedRole = message.guild.roles.find("name", "Muted");
     let unmuteMember = message.mentions.members.first();
     if(!unmuteMember) {
-      message.reply("That's an invalid user.");
+      deleteBotMsg("That's not a valid user")
       return;
     }
     if(unmuteMember.length === 0) {
-      message.reply("Please specify a user to mute.");
+      deleteBotMsg("Please specify a user to mute.")
     }
     if(!message.member.roles.has(modRole.id||adminRole.id) && !message.author.id === message.guild.ownerID) {
-      message.reply("You don't have the sufficient permissions")
+      deleteBotMsg("You don't have the sufficient permissions.")
       return;
     }
     if(!message.guild.member(bot.user).hasPermission("MANAGE_ROLES")) {
-      message.reply("I don't have the sufficient permissions");
+      deleteBotMsg("I don't have the sufficient permissions.")
     }
     unmuteMember.removeRole(mutedRole);
-    message.reply(`Successfully unmuted ${unmuteMember}.`)\n.then(m => { m.delete(3000) })
+    deleteBotMsg("Successfully unmuted " + unmuteMember)
+  }
+
+  if(command === commands[6]) {
+    if (isNaN(args.join(" "))) {
+        deleteBotMsg("That's not a valid number.")
+
+        return;
+    }
+    if (content < 2) {
+        deleteBotMsg("The number provided must be atleast 2.")
+
+        return;
+    }
+    if (content > 100) {
+        deleteBotMsg("The number provided must be below 100.")
+
+        return;
+    }
+    message.channel.bulkDelete(args.join(" ")) + 2;
+    deleteBotMsg("Successfully cleared " + args.join(" "))
   }
 
 });
