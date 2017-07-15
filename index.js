e4e7e40b02eb14483531316e49404655fc26af2e
seanamander <brohonch@gmail.com>
2017-07-15 02:53:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const Discord = require("Discord.js");
 const bot = new Discord.Client();
+var commands = ["%help", "newchannel"];
+var blue = 3447003;
 
 bot.on('ready', () => {
     console.log('loaded BOT'); //logs in the console the bot has loaded
 });
 
 bot.on('ready', () => {
-    bot.user.setGame('%help') //sets the game to %help on launch
+    bot.user.setGame('%help'); //sets the game to %help on launch
 })
 
 bot.on('messageDelete', message => {
@@ -17,7 +19,7 @@ bot.on('messageDelete', message => {
     let guild = message.guild;
     let content = args.join(" ");
     let user = message.author.username;
-    console.log(date + serverName.toUpperCase() + "[DELETED]: " + user + ": " + message.content)
+    console.log(date + serverName.toUpperCase() + "[DELETED]: " + user + ": " + message.content);
 })
 
 bot.on('message', message => {
@@ -26,15 +28,36 @@ bot.on('message', message => {
     var date = "[" + now + "]: ";
     var serverName = "[" + message.guild.name + "]: ";
     let guild = message.guild;
-    let content = args.join(" ")
-    let user = message.author.username
-    console.log(date + serverName.toUpperCase() + user + ": " + message.content)
+    let content = args.join(" ");
+    let user = message.author.username;
+    console.log(date + serverName.toUpperCase() + user + ": " + message.content);
 
   if(message.author.bot) return;
   let command = message.content.split(" ")[0];
-  if(command === "test") {
-    message.reply("worked");
+
+  if(command === commands[0]) { //works but crashes when after execution
+  const embed = new Discord.RichEmbed()
+    .setTitle("Help/Commands")
+    .setAuthor("Vandercoot")
+    .setFooter("Requested by " + message.author.username)
+    .setTimestamp()
+    .addField("Command help", "Say `%help%<command>`")
+    .addField("Example", "`%help%kick`")
+    .setColor(blue)
+    message.author.send({
+      embed
+    });
   }
+
+  if(command === commands[1]) { //not working
+    if(args.length == 1) {
+      let name = args[0];
+      guild.createChannel(name, "text").then(console.log("Successfully created channel " + name)).catch(console.error);
+    } else {
+      message.reply("Invalid usage, try %help.");
+    }
+  }
+
 });
 
 bot.login("MzM0MDk3NzYzNDYzNTkzOTg0.DEWQTg.CCEmUGc0KJKkKfX9iIy8zOI4ymE");
